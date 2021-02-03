24f0d4ff3377db0f8775465e5cb39ddef8504022
Mani kandan B <mani-4980@mani-4980.csez.zohocorpin.com>
2021-02-03 20:16:44 +0530

    AOC Day1 2020

---
+++ 2020/java/src/com/advent/days/Day1.java
@@ -0,0 +1,45 @@
+package com.advent.days;
+
+import com.util.Utils;
+import java.util.List;
+import java.io.File;
+
+public class Day1 {
+
+	public static void main(String[] args) {
+		String path = "AdventOfCode/2020/data/day1.txt";
+		List<Integer> intList = Utils.convertFileToIntList(new File(path));
+		System.out.println(getProduct(intList));
+		System.out.println(get3Product(intList));
+	}
+
+	public static int getProduct(List<Integer> intList) {
+
+		for(int val: intList) {
+			
+			int reqVal = 2020 - val;
+			if(intList.contains(reqVal)) {
+				System.out.println(val + " + " + reqVal);
+				return val * reqVal;
+			}
+		}
+
+		return 0;
+	}
+
+	public static int get3Product(List<Integer> intList) {
+
+		for(int i = 0; i < intList.size(); i++) {
+			for(int j = i + 1 ; j < intList.size(); j++) {
+				int reqVal = 2020 - (intList.get(i) + intList.get(j));
+				if(intList.contains(reqVal)) {
+					System.out.println( intList.get(i) + " + " + intList.get(j) + " + " + reqVal);
+					return intList.get(i) * intList.get(j) * reqVal;
+				}
+			}
+		}
+
+		return 0;
+	}
+
+}
---
+++ 2020/java/src/com/util/Utils.java
@@ -0,0 +1,32 @@
+package com.util;
+
+import java.util.function.Function;
+import java.nio.file.Paths;
+import java.nio.file.Files;
+import java.io.IOException;
+import java.io.File;
+import java.nio.charset.StandardCharsets;
+import java.util.stream.Stream;
+import java.util.stream.Collectors;
+import java.util.List;
+
+public class Utils {
+
+	public static List<Integer> convertFileToIntList(File file) {
+		return getFileContent(file, (stream) -> stream.mapToInt(Integer::parseInt).boxed().collect(Collectors.toList()));
+	}
+
+	public static List<String> convertFileToStringList(File file) {
+		return getFileContent(file, (stream) -> stream.collect(Collectors.toList()));
+	}
+
+	public static <V> V getFileContent(File file, Function<Stream<String>,V> processor) {
+
+		try (Stream<String> lines = Files.lines(Paths.get(file.getAbsolutePath()), StandardCharsets.UTF_8)) {
+			return processor.apply(lines);
+		} catch(IOException e) {
+			e.printStackTrace();
+			return null;
+		}
+	}
+}
